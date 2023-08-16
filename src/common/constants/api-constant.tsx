6033eeb09413c74f845d4768aac0f2c41a73744a
Neon Nguyen <neon.n@itcgroup.io>
2023-08-16 22:28:31 +0700

    Added SIT URL for prod build

--- src/common/constants/api-constant.tsx
+++ src/common/constants/api-constant.tsx
@@ -1,4 +1,4 @@
-export const LOCAL_BASE_URL = 'http://localhost:3001/';
+export const LOCAL_BASE_URL = 'https://hungry-hub-back-end-service.onrender.com/';
 export const LOCAL_USERS_CONTROLLER = 'users';
 export const LOCAL_CATEGORIES_CONTROLLER = 'categories';
 export const LOCAL_PRODUCTS_CONTROLLER = 'products';
