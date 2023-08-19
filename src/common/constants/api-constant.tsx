9d49f758f2365613d5daa61849bafae583b07d09
Neon Nguyen <neon.n@itcgroup.io>
2023-08-19 16:41:46 +0700

    Init build for chatbot

--- src/common/constants/api-constant.tsx
+++ src/common/constants/api-constant.tsx
@@ -1,4 +1,6 @@
 export const LOCAL_BASE_URL = 'https://hungry-hub-back-end-service.onrender.com/';
+export const SIT_BASE_URL = 'http://localhost:3001/';
 export const LOCAL_USERS_CONTROLLER = 'users';
 export const LOCAL_CATEGORIES_CONTROLLER = 'categories';
 export const LOCAL_PRODUCTS_CONTROLLER = 'products';
+export const CHATBOT_SIT_URL = 'https://hungry-hub-chatbot-service.onrender.com/predict'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,8 +21,57 @@ import { fetchCategories } from '@framework/category/get-all-categories';
 import { fetchBestSellerGroceryProducts } from '@framework/product/get-all-best-seller-grocery-products';
 import { fetchPopularProducts } from '@framework/product/get-all-popular-products';
 import { LIMITS } from '@framework/utils/limits';
-
+import dynamic from 'next/dynamic';
+import 'react-chat-widget/lib/styles.css';
+import { CHATBOT_SIT_URL } from 'src/common/constants/api-constant';
+export interface ResponseData {
+  answer: string;
+}
 export default function Home() {
+  const isSSR = typeof window === 'undefined';
+  const imagePath = '/assets/images/linkicon.png'; // Adjust the path as needed
+
+  const DynamicWidget = dynamic(
+    () => import('react-chat-widget').then((mod) => mod.Widget),
+    {
+      ssr: false, // Load only on the client side
+    }
+  );
+
+  const handleNewUserMessage = (newMessage: string) => {
+    console.log(`New message incoming! ${newMessage}`);
+
+    // Send the user message to your API
+    fetch(CHATBOT_SIT_URL, {
+      method: 'POST',
+      body: JSON.stringify({ message: newMessage }),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json', // Add this line to indicate JSON response is accepted
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Add the response from the API to the chat UI
+        const responseData = data as ResponseData;
+        addResponseToWidget(responseData.answer);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        //Handle error and display an error message in the chat UI
+        addResponseToWidget(
+          'Oops! Something went wrong. Please try again later.'
+        );
+      });
+  };
+
+  const addResponseToWidget = (text: string) => {
+    if (typeof window !== 'undefined') {
+      const { addResponseMessage } = require('react-chat-widget');
+      addResponseMessage(text);
+    }
+  };
+
   return (
     <>
       <Seo
@@ -39,6 +88,15 @@ export default function Home() {
           className="mb-12 lg:mb-14 xl:mb-16 2xl:mb-20"
           data={bundle}
         />
+        {process.browser && !isSSR && (
+          <DynamicWidget
+            handleNewUserMessage={handleNewUserMessage}
+            title="Hungry Hub - Chatbot Service"
+            subtitle="Hi how can I help you today?"
+            showTimeStamp={true}
+            profileAvatar={imagePath}
+          />
+        )}
         <CategoryGridBlock />
         <BestSellerGroceryProductFeed />
         <BannerCard
