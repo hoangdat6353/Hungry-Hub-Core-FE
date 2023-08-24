a6f3345dd268d2c93b34cf34e41a99e3afa31365
Neon Nguyen <neon.n@itcgroup.io>
2023-08-25 00:46:46 +0700

    Update prod build path

--- src/common/constants/api-constant.tsx
+++ src/common/constants/api-constant.tsx
@@ -1,6 +1,8 @@
-export const LOCAL_BASE_URL = 'http://localhost:3001/'; 
-export const SIT_BASE_URL = 'https://hungry-hub-back-end-service.onrender.com/';
+export const LOCAL_BASE_URL =
+  'https://hungry-hub-back-end-service.onrender.com/';
+export const SIT_BASE_URL = 'http://localhost:3001/';
 export const LOCAL_USERS_CONTROLLER = 'users';
 export const LOCAL_CATEGORIES_CONTROLLER = 'categories';
 export const LOCAL_PRODUCTS_CONTROLLER = 'products';
-export const CHATBOT_SIT_URL = 'https://hungry-hub-chatbot-service.onrender.com/predict'
+export const CHATBOT_SIT_URL =
+  'https://hungry-hub-chatbot-service.onrender.com/predict';
