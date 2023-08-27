85e0826757754677df727bc101d0371be132dd84
Neon Nguyen <neon.n@itcgroup.io>
2023-08-27 22:17:45 +0700

    Update VNFlag

--- src/components/icons/language/VNFlag.tsx
+++ src/components/icons/language/VNFlag.tsx
@@ -1,7 +1,7 @@
 export const VNFlag: React.FC<React.ImgHTMLAttributes<{}>> = ({ ...rest }) => {
   return (
     <img
-      src="/assets/images/vn.svg"
+      src="/assets/images/VNFlag.svg"
       alt="SVG VN Flags"
       style={{ height: '100%', width: 'auto' }}
       {...rest}
--- src/components/ui/language-switcher.tsx
+++ src/components/ui/language-switcher.tsx
@@ -29,7 +29,7 @@ export default function LanguageSwitcher() {
         <div className="relative z-10 lg:top-[1px]">
           <Listbox.Button className="text-skin-base relative w-full py-2 ps-3 pe-5 text-start  rounded-lg shadow-md focus:outline-none cursor-pointer">
             <span className="flex truncate items-center text-sm lg:text-15px">
-              <span className="me-2 w-5 h-5 rounded-full overflow-hidden flex-shrink-0">
+              <span className="me-2 w-5 h-5 rounded-sm overflow-hidden flex-shrink-0">
                 {selectedItem.icon}
               </span>
               <span className="leading-5 pb-0.5">{t(selectedItem.name)}</span>
